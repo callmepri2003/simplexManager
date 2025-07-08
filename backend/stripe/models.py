0319c1aab516dd516670156ae64a06d1b64bef44
callmepri <140776053+callmepri@users.noreply.github.com>
2025-07-08 12:43:15 +1000

    feat: Stripe product model

--- backend/stripe/models.py
+++ backend/stripe/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class StripeProd(models.Model):
+  stripeId=models.CharField(max_length=20, blank=False, null=False)
+  name=models.CharField(max_length=100, blank=False, null=False)
