ce13683c587b5d9db6ac345823a56488ed4f9a7d
callmepri <140776053+callmepri@users.noreply.github.com>
2025-07-08 15:30:19 +1000

    feat: failed signature test

--- backend/stripeInt/tests.py
+++ backend/stripeInt/tests.py
@@ -10,7 +10,7 @@ class WebHooksTest(TestCase):
     self.url = "/stripe/webhooks/" 
 
   @patch("stripe.Webhook.construct_event")
-  def testProductCreated(self, mock_construct_event):
+  def testProductCreated_HappyPath(self, mock_construct_event):
     randomId = "prod_d73hg92hg82hgg0"
     mock_construct_event.return_value = {
       'type': 'product.created',
@@ -32,28 +32,27 @@ class WebHooksTest(TestCase):
     product = StripeProd.objects.get(stripeId=randomId)
     self.assertIsNotNone(product)
     self.assertTrue(product.name, "testProduct1")
+  
+  @patch("stripe.Webhook.construct_event")
+  def testProductCreated_SignatureFailed(self, mock_construct_event):
+    randomId = "prod_d73hg92hg82hgg0"
+    mock_construct_event.return_value = {
+      'type': 'product.created',
+      'data': {
+        'object' : {
+          'id': randomId,
+          'name': 'testProduct1'
+        }
+      }
+    }
 
-  # @patch("stripe.Webhook.construct_event")
-  # def testProductUpdated(self, mock_construct_event):
-  #   randomId = "prod_d73hg92hg82hgg0"
-  #   mock_construct_event.return_value = {
-  #     'type': 'product.updated',
-  #     'data': {
-  #       'object' : {
-  #         'id': randomId,
-  #         'name': 'testProduct1'
-  #       }
-  #     }
-  #   }
+    mock_construct_event.side_effect = stripe.error.SignatureVerificationError("Signature could not be verified", "Header")
 
-  #   res = self.client.post(
-  #     self.url,
-  #     data=b"{}",  # Doesn't matter – it's ignored due to mocking
-  #     content_type="application/json",
-  #     HTTP_STRIPE_SIGNATURE="fake_signature"
-  #   )
-
-  #   product = StripeProd.objects.get(stripeId=randomId)
-  #   self.assertIsNotNone(product)
-  #   self.assertTrue(product.name, "testProduct1")
-    
+    res = self.client.post(
+      self.url,
+      data=b"{}",  # Doesn't matter – it's ignored due to mocking
+      content_type="application/json",
+      HTTP_STRIPE_SIGNATURE="fake_signature"
+    )
+    with self.assertRaises(StripeProd.DoesNotExist):
+      product = StripeProd.objects.get(stripeId=randomId)
