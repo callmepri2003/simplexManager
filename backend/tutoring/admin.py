0be01e0c49e79a835dbf211fa333c8fd785c2fcc
callmepri <140776053+callmepri@users.noreply.github.com>
2025-07-09 07:18:42 +1000

    feat: models for customers subscriptions

--- backend/tutoring/admin.py
+++ backend/tutoring/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Group)
+admin.site.register(Lesson)
+admin.site.register(Attendance)
+admin.site.register(Parent)
+admin.site.register(Student)
+admin.site.register(Basket)
+admin.site.register(BasketItem)
--- backend/tutoring/models.py
+++ backend/tutoring/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from stripeInt.models import StripeProd
 
 class Group(models.Model):
   lesson_length = models.IntegerField(default=1)
+  associated_product = models.ForeignKey(StripeProd, on_delete=models.CASCADE, related_name="subscribed_groups", null=True)
 
 class Lesson(models.Model):
   group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='lessons')
@@ -10,9 +12,18 @@ class Attendance(models.Model):
   lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='attendances')
   invoiced = models.BooleanField(default=False)
 
+class Basket(models.Model):
+  None
+
+class BasketItem(models.Model):
+  basket = models.ForeignKey(Basket, on_delete=models.CASCADE, related_name='items')
+  product = models.ForeignKey(StripeProd, on_delete=models.CASCADE, related_name="basketItems")
+  quantity = models.IntegerField()
+
 class Parent(models.Model):
   name = models.CharField(max_length=100)
   stripeId = models.CharField(max_length=100)
+  basket = models.OneToOneField(Basket, on_delete=models.DO_NOTHING, null=True)
   is_active = models.BooleanField(default=True)
 
 class Student(models.Model):
