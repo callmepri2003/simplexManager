72bc8a5303bc4fc4daaaad6e4eba846969adf894
callmepri <140776053+callmepri@users.noreply.github.com>
2025-07-11 10:42:06 +1000

    feat: getting ready for heroku

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
@@ -142,3 +142,5 @@ LOGGING = {
         },
     },
 }
+
+STATIC_ROOT = 'staticfiles'
